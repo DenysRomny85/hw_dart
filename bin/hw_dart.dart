52b5347fee57a7ed5a309392d060383a90aef1e4
DenysOnyschenko <denis.romny@gmail.com>
2026-03-11 11:01:56 +0200

    init commit

---
+++ bin/hw_dart.dart
@@ -0,0 +1,5 @@
+import 'package:hw_dart/hw_dart.dart' as hw_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hw_dart.calculate()}!');
+}
---
+++ test/hw_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:hw_dart/hw_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
