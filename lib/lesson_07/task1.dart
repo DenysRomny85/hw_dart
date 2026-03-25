1f5fa0a061925720d1ecfe4d65a7fa2137129429
DenysOnyschenko <denis.romny@gmail.com>
2026-03-25 08:24:11 +0200

    tast 1

---
+++ lib/lesson_07/task1.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+void main() {
+  final list = List.generate(100, (index) => Random().nextInt(101));
+  print(list);
+  print('65-й елемент: ${list[65]}');
+  list[50] = 1000000000;
+  print('50-й елемент: ${list[50]}');
+
+  final remoteList = {24, 45, 66, 88};
+  list.removeWhere(remoteList.contains);
+  print(list);
+
+  var sum = 0;
+  for (var index = 0; index < list.length; index++) {
+    if (list[index] % 3 == 0) {
+      sum += list[index];
+    }
+  }
+  print('sum: $sum');
+
+  final temp = <int>[];
+  for (final el in list) {
+    if (el % 2 == 0) {
+      temp.add(el);
+    }
+  }
+  print('length temp: ${temp.length}');
+  print(temp);
+}
